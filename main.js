da6b57823911342b119877b9d3a559bdeeac42a9
Michelle Lee <lee.michellejung@gmail.com>
2017-02-21 09:23:01 -0800

    finishes challenge-- I 'cheated' by hard coding button and input field in html

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-main.js
+var count = 0
+$('#countBtn').on('click', function(evt){
+  $('#countBtn').text(count++)
+})
+
+
+$('#colorInput').on('keypress', function(evt){
+  if(evt.keyCode===13){
+    $('body').css('background-color', $('#colorInput').val())
+    $('#colorList').append(`<li>${$('#colorInput').val()}</li>`)
+  }
+})
